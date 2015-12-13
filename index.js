9e0603c883d39bad17a48774ebec4d92654c6bdc
Evgeniy Gavryushin <job.egavr@yandex.ru>
2015-12-13 15:55:16 +0300

    Ok! Let's go!

---
+++ index.js
@@ -0,0 +1,17 @@
+var Checker = require('jscs'),
+    checker = new Checker();
+
+///
+exports.forEntityTech = function(tech, techConfig, entity) {
+    checker.configure(techConfig);
+
+    var results = checker.checkString(tech.content);
+
+    results.getErrorList().forEach(function(error) {
+        entity.addError({
+            msg: error.message,
+            tech: tech.name,
+            value: results.explainError(error, true).replace(/^.*?\n/, '')
+        });
+    });
+};
